86cf6b04991c781a703d0cf3593a4db358c35285
Wil Burke <billyburke13@gmail.com>
2016-03-06 14:52:32 -0500

    Premium Users' wikis all become private when they downgrade their status | Standard users cannot see private wikis

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
 
   def downgrade
+    current_user.update_attributes(role: 0)
 
+    current_user.wikis.each do |wiki|
+      wiki.update_attributes(private: false)
+    end
     redirect_to root_path
     flash[:notice] = "Your Premium status has been changed to Standard. All of your Wikis are now public."
   end
