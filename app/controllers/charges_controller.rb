4d50c7aeced821f31614abc1308540bd1ea21cf2
Wil Burke <billyburke13@gmail.com>
2016-03-06 13:01:13 -0500

    User can pay to upgrade and elect to downgrade his status

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -9,48 +9,34 @@ class ChargesController < ApplicationController
   end
 
   def create
-    # I opted to use a subscription over a single charge plan, but this is the code
-    # for a single charge
 
-    # charge = Stripe::Charge.create(
-    # customer: customer.id, # Note -- this is NOT the user_id in your app
-    # amount: Amount.default,
-    # description: "BigMoney Membership - #{current_user.email}",
-    # currency: 'usd'
-    # )
-      customer = Stripe::Customer.create(
-      email: current_user.email,
-      plan: '1111',
-      card: params[:stripeToken],
-      customer: customer.id
-      )
+    customer = Stripe::Customer.create(
+    email: current_user.email,
+    card: params[:stripeToken]
+    )
 
-      current_user.subscribed = true
-      current_user.stripeid = customer.id
-      subscription =
-      current_user.save
+    charge = Stripe::Charge.create(
+    customer: customer.id, # Note -- this is NOT the user_id in your app
+    amount: 1500,
+    description: "Premium Membership - #{current_user.email}",
+    currency: 'usd'
+    )
 
-    if current_user.subscribed?
+    if charge.present?
       current_user.update_attributes(role: 2)
     end
+
     flash[:notice] = "Thanks for all the money, #{current_user.email}! Feel free to pay me again."
     redirect_to root_path
   end
 
-  def destroy
-
-    # # customer = Stripe::Charge.retrieve(id: "ch_17llMsFVfN4XkAreP25C8hkJ", expand: ['customer'])
-    #
-    # customer = Stripe::Customer.retrieve({current_user.stripeid})
-    # customer.subscriptions.retrieve({SUBSCRIPTION_ID}).delete
-  end
 
   # Stripe will send back CardErrors, with friendly messages
   # when something goes wrong.
   # This `rescue block` catches and displays those errors.
-  rescue Stripe::CardError => e
-    flash.now[:alert] = e.message
-    redirect_to new_charge_path
+rescue Stripe::CardError => e
+  flash.now[:alert] = e.message
+  redirect_to new_charge_path
 
 
 end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,17 @@
+class UsersController < ApplicationController
+
+  def downgrade
+    current_user.update_attributes(role: 1)
+
+    current_user.wikis.each do |wiki|
+      wiki.update_attributes(private: false)
+    end
+    redirect_to root_path
+    flash[:notice] = "Your Premium status has been changed to Standard. All of your Wikis are now public."
+  end
+
+  def keep
+    redirect_to root_path
+    flash[:notice] = "Your Premium status remains intact."
+  end
+end
