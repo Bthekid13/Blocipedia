96a801eb5f3ac7db104609739fadc9db710a20ab
Wil Burke <billyburke13@gmail.com>
2016-03-06 14:24:21 -0500

    premium users have private wikis

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,8 +10,8 @@ class UsersController < ApplicationController
     flash[:notice] = "Your Premium status has been changed to Standard. All of your Wikis are now public."
   end
   #
-  # def keep
-  #   redirect_to root_path
-  #   flash[:notice] = "Your Premium status remains intact."
-  # end
+  def keep
+    redirect_to root_path
+    flash[:notice] = "Your Premium status remains intact."
+  end
 end
