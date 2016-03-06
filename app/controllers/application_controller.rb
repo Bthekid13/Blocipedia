193fefb33e6ac46e8ffe8888a066429c1a1a70e3
Wil Burke <billyburke13@gmail.com>
2016-03-05 22:44:54 -0500

    update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,18 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  before_action :role_change
   include Pundit
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
   protect_from_forgery with: :exception
 
+  def role_change
+    ActiveRecord::Base.transaction do
+      if User.update_attributes(role: 1)
+        User.update_attributes(private: false)
+      end
+    end
+  end
 
   private
   #
@@ -12,4 +20,8 @@ class ApplicationController < ActionController::Base
     flash[:alert] = "You must be an admin to do that"
     redirect_to (request.referrer || root_path )
   end
+
+
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,15 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   #:lockable, :timeoutable and :omniauthable
-  enum role: [:admin, :standard, :premium ]
   after_initialize :set_default_role, :if => :new_record?
-
-  def set_default_role
-    self.role ||= :standard
-  end
 
   has_many :wikis
   devise :database_authenticatable, :registerable, :recoverable,
          :rememberable, :trackable, :validatable, :confirmable
 
+  enum role: [:admin, :standard, :premium ]
 
-
-
-
-
+  def set_default_role
+    self.role ||= :standard
+  end
 end
